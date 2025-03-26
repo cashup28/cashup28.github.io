aa9d60ab2ea209c9e3c57790daff2e5afb5ac389
cashup28 <acyedek1@gmail.com>
2025-03-27 01:03:57 +0300

    Updates

---
+++ tonconnect-manifest.js
@@ -0,0 +1,7 @@
+{
+  "url": "https://cashup28.github.io/",
+  "name": "MonadP2P",
+  "termsOfUseUrl": "https://cashup28.github.io/terms.html",
+  "privacyPolicyUrl": "https://cashup28.github.io/privacy.html",
+  "iconUrl": "https://cashup28.github.io/icon.png"
+}
